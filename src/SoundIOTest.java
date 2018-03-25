b37c6eb9703fde7c2c2c6c291e477937113b4e2c
Peter Rong <PeterRong96@gmail.com>
2018-03-25 19:41:44 +0800

    added a test file.
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>

---
+++ src/SoundIOTest.java
@@ -0,0 +1,23 @@
+import AcousticNetwork.SoundIO;
+class SoundIOTest{
+	// Try modify this to test.
+	public static void main(String[] args) throws Exception{
+		double dur = 4;
+		int sample_rate = 44100;
+		int sample_cnt = (int) (dur * sample_rate);
+		double[] wave = new double[sample_cnt];
+		for (int i=0; i<sample_cnt; i++){
+			double t = (double) i / sample_rate;
+			wave[i] = 
+				0.5*(Math.sin(2*Math.PI*1000*t) + Math.sin(2*Math.PI*10000*t));
+		}
+		SoundIO sound_io = new SoundIO(sample_rate);
+
+		String path = "../wav/record1_41.wav";
+		sound_io.play_file(path);
+		//sound_io.sound(wave);
+		wave = sound_io.record(100000);
+		//sound_io.sound(wave);
+		//sound_io.save_file(wave, "t.wav");
+	}
+}
