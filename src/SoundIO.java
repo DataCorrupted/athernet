1f2669a9c96152734ecda231bde09d2600dc4eec
Peter Rong <PeterRong96@gmail.com>
2018-03-25 12:25:55 +0800

    play file done.
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>

--- src/SoundIO.java
+++ src/SoundIO.java
@@ -2,44 +2,80 @@ import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.sound.sampled.SourceDataLine;
-
+import javax.sound.sampled.AudioInputStream;
+import java.io.File;
+import java.io.IOException;
 import java.nio.ByteBuffer;
 
 class SoundIO{
-	private int freq;
+	private int sample_rate_;
+	private AudioFormat format_;
+	private DataLine.Info o_info_;
+	private SourceDataLine o_line_;
+	private final int FRAMESIZE = 2;
+	private final int BYTESIZE = 8;
 
-	public static void play(ByteBuffer out) throws InterruptedException, LineUnavailableException {
-
-		AudioFormat format = new AudioFormat(44100, 16, 1, true, true);
-		DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
-		SourceDataLine line;
-		if (!AudioSystem.isLineSupported(info)){
-			System.out.println("Line matching " + info + " is not supported.");
+	public SoundIO(int sr) throws LineUnavailableException{
+		this.sample_rate_ = sr;
+		this.format_ = new AudioFormat(44100, FRAMESIZE * BYTESIZE, 1, true, true);
+		this.o_info_ = new DataLine.Info(SourceDataLine.class, format_);
+		if (!AudioSystem.isLineSupported(this.o_info_)){
+			System.out.println(
+				"Line matching " + this.o_info_ + " is not supported.");
 			throw new LineUnavailableException();
 		}
+		this.o_line_ = (SourceDataLine)AudioSystem.getLine(this.o_info_);
+	}
 
-		line = (SourceDataLine)AudioSystem.getLine(info);
-		line.open(format);  
-		line.start();
 
-		line.write(out.array(), 0, 22050);
-		line.write(out.array(), 0, 22050);
+	private void play(ByteBuffer out) throws LineUnavailableException {
+		o_line_.open(this.format_);
+		o_line_.start();
+		o_line_.write(out.array(), 0, out.capacity());
+		// Drain every data in the buffer before it's closed.
+		o_line_.drain();
+		// Close it.
+		o_line_.close();
+	}
 
-		line.drain();                                         
-		line.close();
+	public void play_file(String path)
+		throws UnsupportedAudioFileException, IOException, LineUnavailableException{
+		File f = new File(path);
+		AudioInputStream src = AudioSystem.getAudioInputStream(f);
+		AudioFormat src_format = src.getFormat();
+		AudioInputStream dst = AudioSystem.getAudioInputStream(this.format_, src);
+
+		int byte_cnt = (int) dst.getFrameLength() / src_format.getFrameSize() * FRAMESIZE;
+
+		byte[] bs = new byte[byte_cnt];
+		int r = dst.read(bs, 0, byte_cnt);
+		
+		ByteBuffer out = ByteBuffer.wrap(bs);
+
+		play(out);
+
 	}
 
 	// Try modify this to test.
 	public static void main(String[] args) throws Exception{
-		double dur = 0.5;
+		double dur = 10;
 		int sample_rate = 44100;
 		int sample_cnt = (int) (dur * sample_rate);
-		System.out.println(Short.MAX_VALUE);
 		ByteBuffer out = ByteBuffer.allocate(sample_cnt*2);
 		for (int i=0; i<sample_cnt; i++){
-			out.putShort((short)(Short.MAX_VALUE * Math.cos(2*Math.PI * 15000 * i/sample_rate)));
+			double t = (double) i / sample_rate;
+			double data = 
+				Math.sin(2*Math.PI*1000*t) + Math.sin(2*Math.PI*10000*t);
+			out.putShort((short)(Short.MAX_VALUE * data));
 		}
-		play(out);
+		SoundIO sound_io = new SoundIO(sample_rate);
+
+		String path = "../wav/record1_41.wav";
+		System.out.println(path);
+		//sound_io.play_file(path);
+		
+		sound_io.play(out);
 	}
 }
