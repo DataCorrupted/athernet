4b9632ef2f5823159d23d8e099ce0808158d9140
Peter Rong <PeterRong96@gmail.com>
2018-03-31 23:18:45 +0800

    changed server. About to write client
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>

--- src/Server.java
+++ src/Server.java
@@ -1,16 +1,42 @@
-// import AcousticNetwork.Receiver;
+import AcousticNetwork.Receiver;
+import AcousticNetwork.Transmitter;
+import AcousticNetwork.FileI;
+import AcousticNetwork.SoundO;
 // import AcousticNetwork.Sender;
 
 public class Server {
 
+    private CRC8 crc8_;
+    private FileI = i_file_;
     private Receiver receiver_;
     private Transmitter transmitter_;
 
-    public static void main(String[] args){
-        // --------------- initial file transmission ------------------
-        // parse the parameters (if needed)
+    public Server(String file, int file_format){
+        receiver_ = new Receiver();
+        transmitter_ = new Transmitter();
+        i_file_ = FileI(file, file_format);
+        o_sound_ = SoundO();
+    }
 
-        // open the file
+    public static void main(String[] args){
+        // Parsing args.
+        int i= 0;
+        String file = "./I"
+        while (i<args.length){
+            if (args[i].equals("-f")){
+                i++;
+                if (i == args.length){
+                    System.out.println("No file given, using default.");
+                } else {
+                    file = args[i];
+                }
+            } else {
+                System.out.println("No such command.");
+            }
+            i++;
+        }
+        // Setup a server.
+        Server server = new Server(file, FileI.TEXT01);
 
         // send out a dummy (empty) packet
 
