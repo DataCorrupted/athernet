9047963c4ecb44bdec524e575e20cc2d88bb3c89
Peter Rong <PeterRong96@gmail.com>
2018-04-20 23:21:06 +0800

    file organized
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>

--- src/AcousticNetwork/FileO.java
+++ src/AcousticNetwork/FileO.java
@@ -2,20 +2,17 @@ package AcousticNetwork;
 
 import java.io.FileOutputStream;
 
-public class FileO{
-	public static final int BIN = 0;
-	public static final int TEXT01 = 1;
-
-	private int file_format_;
+public class FileO extends FileIO{
 	private FileOutputStream o_file_; 
 
 	public FileO() throws Exception{
-		this("./O", 1);
+		this("./O", TEXT01);
 	}
 	public FileO(String path, int file_format) throws Exception{
-		file_format_ = file_format;
-		o_file_ = new FileOutputStream(path);
+		super(path, file_format);
+		o_file_ = new FileOutputStream(file_);
 	}
+
 	public void write(byte[] f, int offset, int len) throws Exception{
 		if (len <= 0) { return; }
 
@@ -25,15 +22,5 @@ public class FileO{
 			f = bitsToText01(f);
 		}
 		o_file_.write(f, offset, len);
-	}
-	private byte[] bitsToText01(byte[] bytes){
-		byte[] out = new byte[bytes.length << 3];
-		for (int i=0; i<bytes.length; i++){
-			for (int j=0; j<8; j++){
-				out[(i<<3)+7-j] = (byte) (((bytes[i] & 1) == 1)? '1':'0');
-				bytes[i] = (byte) (bytes[i] >>> 1);
-			}
-		}
-		return out;
 	}
 }
