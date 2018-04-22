14a494638d24ce81f6e7bbe2c39ae3a863190748
Peter Rong <PeterRong96@gmail.com>
2018-04-22 15:37:22 +0800

    mac framework done
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>

---
+++ src/athernet/mac/MacLayer.java
@@ -0,0 +1,47 @@
+package athernet.mac;
+
+class MacLayer{
+	// The caller of MacLayer should assign a Mac Address.
+	public byte address_;
+
+	// The maximum resend time before we rule a link failure.
+	public int max_resend_;
+
+	// Time we wait until we rule a timeout and resend the same pack.
+	public double timeout_;
+
+	// Status of the MacLayer
+	public int status_;
+	public final int IDLEMAC = 0;
+	public final int BUSYMAC = 1;
+	public final int EMPTTXQ = 2;
+	public final int EMPTRXQ = 3;
+	public final int LINKERR = -1;
+
+	public MacLayer(byte address){
+		this(address, 0.1, 3);
+	}
+	public MacLayer(byte address, double timeout, int max_resend){
+		address_ = address;
+		max_resend_ = max_resend;
+		timeout_ = timeout;
+	}
+
+	public void requestSend(byte[]){
+		return;
+	}
+
+	private void send(){
+		return;
+	}
+
+	private void receive(){
+		return;
+	}
+
+	private void timer(){
+		return;
+	}
+
+	public void getStatus(){ return status_; }
+}
