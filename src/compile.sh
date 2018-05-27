8fcaf6ae047b81eeb8da0abe6f2cddea1d2f2ba3
Peter Rong <PeterRong96@gmail.com>
2018-05-27 21:33:14 +0800

    compile script changed
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>

--- src/compile.sh
+++ src/compile.sh
@@ -2,3 +2,5 @@ javac -d . athernet/util/*.java
 javac -d . athernet/physical/*.java
 javac -d . athernet/mac/*.java
 javac -d . athernet/tools/*.java
+javac -d . com/savarese/rocksaw/net/*.java
+javac -d . org/savarese/vserv/tcpip/*.java
