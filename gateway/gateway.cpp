4f5c2996f3de0b085ca6620e9f588dca8b933d2c
Peter Rong <PeterRong96@gmail.com>
2018-06-02 08:56:55 +0800

    mix pipe done
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>

--- gateway/gateway.cpp
+++ gateway/gateway.cpp
@@ -11,8 +11,7 @@ using namespace std;
 int main(int argc, char *argv[]){
 	string str;
 	cout << argc << endl;
-	cerr << argv[0];
-	cin >> str;
-	cerr << "I am " << argv[0] << ". I received str from " << str << endl;
+	getline(cin, str);
+	cerr << "I am cpp. I received a str from Java:\n  " << str << endl;
 	return 0;
 }
--- src/athernet/mac/MacInterface.java
+++ src/athernet/mac/MacInterface.java
@@ -2,12 +2,17 @@ package athernet.mac;
 
 import athernet.mac.MacLayer;
 import athernet.mac.MacPacket;
+import java.io.*;
 
-class MacReceiver{
-	static public void main(String[] args){
+class MacInterface{
+	static public void main(String[] args) throws Exception{
 		if (args.length == 0){
 			System.out.println("No input. You have to give me something!");
 		} else {
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+			String line;
+			line = in.readLine();
+			System.err.println("I am Java, I received a str from cpp:\n  " + line);
 			for (int i=0; i<args.length; i++){
 				System.out.print(args[i] + " ");
 			}
