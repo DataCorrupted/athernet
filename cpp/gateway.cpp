a8445ad0afcabc732b9a257e6673c6e4038e80ee
Jianxiong Cai <caijx@shanghaitech.edu.cn>
2018-06-02 17:53:50 +0800

    updated

--- cpp/gateway.cpp
+++ cpp/gateway.cpp
@@ -13,11 +13,12 @@ using namespace std;
 Gateway gateway;
 
 int getUnsignedByte(){
-    return (int)(getchar() & 0xff);
+    int tmp = (int)(getchar() & 0xff);
+    return tmp;
 }
 void send(){
     // Receive a pack from athernet.
-    int len = getUnsignedByte() << 8 + getUnsignedByte();
+    int len = (getUnsignedByte() << 8) + getUnsignedByte();
     int data[len];
     for (int i=0; i<len; i++){
         data[len] = getUnsignedByte();
@@ -30,12 +31,15 @@ void send(){
     }
     std::string encoded_frame = std::string(buf,len);
 
+    std::cerr << "[DEBUG, gateway] received frame of length: " << len << std::endl;
+
     // Make a UDP Packet.
     // IP: data[0].data[1].data[2].data[3]
     // Port: data[4] << 8 + data[5]
     // Data: data[6:]
     NatPacket nat_pack(encoded_frame);
 
+    std::cerr << "[DEBUG, gateway] decode NAT compelted" << std::endl;
 
     // Send the UDP Packet.
     gateway.nat_send(nat_pack.get_ip(), nat_pack.get_port(), nat_pack.get_content());
