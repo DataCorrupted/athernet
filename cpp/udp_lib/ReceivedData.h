ce7d1704cb99bcdc896fe4b14b8b580f43bb637f
Jianxiong Cai <caijx@shanghaitech.edu.cn>
2018-06-02 23:24:12 +0800

    Change port_num

--- cpp/udp_lib/ReceivedData.h
+++ cpp/udp_lib/ReceivedData.h
@@ -5,20 +5,20 @@ class ReceivedData{
 public:
     ReceivedData(){
         src_ip_ = "";
-        src_port_ = -1;
+        src_port_ = 65535;
         content_ = "";
     }
 
     void set_src_ip(std::string src_ip){src_ip_ = src_ip;}
-    void set_src_port(int src_port){src_port_ = src_port;}
+    void set_src_port(unsigned int src_port){src_port_ = src_port;}
     void set_content(std::string content){content_ = content;}
     std::string get_src_ip(){return src_ip_;}
-    int get_src_port(){return src_port_;}
+    unsigned int get_src_port(){return src_port_;}
     std::string get_content(){ return content_;}
 
 private:
     std::string src_ip_;
-    int src_port_;
+    unsigned int src_port_;
     std::string content_;
 };
 
